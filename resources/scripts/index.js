645b9811f04d1f644eaf5dbdfaa943a3b1337492
Clay Suddarth <claysuddarth@MacBook-Air-6.local>
2023-04-10 13:17:21 -0500

    mona lisa

---
+++ resources/scripts/index.js
@@ -0,0 +1,13 @@
+const form1 = document.getElementById("form1")
+
+form1.addEventListener("submit", function (event){
+    event.preventDefault()
+    const id1 = document.getElementById("username").value
+    const id2 = document.getElementById("password").value
+    if (id1 == "admin@gmail.com" && id2 == "admin123") {
+        window.location.assign("adminhomepage.html");
+    }
+    else {
+        alert("Invalid Information")
+    }
+})
