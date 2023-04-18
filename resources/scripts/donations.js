fa2db6dfe61c9d1c922b3764897de538d1d46d1c
Clay Suddarth <claysuddarth@MacBook-Air-6.local>
2023-04-18 13:06:36 -0500

    committing

--- resources/scripts/donations.js
+++ resources/scripts/donations.js
@@ -1,7 +1,7 @@
 const url = "https://localhost:7060/api/Donations";
 
 function handleAddDonation()
-  {
+{
     let donEmail = document.getElementById("email").value
     let donName = document.getElementById("name").value
     let donAmount = document.getElementById("donate-amount").value
@@ -13,13 +13,39 @@ function handleAddDonation()
         donateAmount: donAmount,
         date: dateToday
     }
-    var response = fetch(url, {
-        method: "POST",
-        body: JSON.stringify(newDonation),
-        headers: {
-        "Content-type": "application/json; charset=UTF-8"  
-        },
-    })
-    // setTimeout(()=> addRow(response.json),2000)
+    try {
+        var response = fetch(url, {
+            method: "POST",
+            body: JSON.stringify(newDonation),
+            headers: {
+            "Content-type": "application/json; charset=UTF-8"  
+            },
+        })
+    } catch(Exception) {
+        console.log("Donation error")
+    }
+    setTimeout(()=> addRow(response.json),2000)
     // location.reload()
-  }
+}
+
+// const createSong = async (event) => {
+//     event.preventDefault();
+//     const target = event.target;
+//     const song = {
+//         title: target.title.value,
+//         artist: target.artist.value,
+//         dateAdded: target.dateAdded.value,
+//     }
+//     await fetch(url, {
+//         method: "POST",
+//         headers: {
+//             accept: "*/*",
+//             "content-type": "application/json",
+//         },
+//         body: JSON.stringify(song),
+//     })
+//     render();
+//     target.title.value = "";
+//     target.artist.value = "";
+//     target.dateAdded.value = "";
+// }
