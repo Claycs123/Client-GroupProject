59483228a7e3193301c21fa408478a89ed73df6a
Clay Suddarth <claysuddarth@MacBook-Air-6.local>
2023-04-18 12:27:21 -0500

    commitment

--- resources/scripts/donations.js
+++ resources/scripts/donations.js
@@ -1,1 +1,25 @@
 const url = "https://localhost:7060/api/Donations";
+
+function handleAddDonation()
+  {
+    let donEmail = document.getElementById("email").value
+    let donName = document.getElementById("name").value
+    let donAmount = document.getElementById("donate-amount").value
+    let dateToday = new Date();
+    const newDonation =
+    {
+        email: donEmail,
+        name: donName,
+        donateAmount: donAmount,
+        date: dateToday
+    }
+    var response = fetch(url, {
+        method: "POST",
+        body: JSON.stringify(newDonation),
+        headers: {
+        "Content-type": "application/json; charset=UTF-8"  
+        },
+    })
+    // setTimeout(()=> addRow(response.json),2000)
+    // location.reload()
+  }
