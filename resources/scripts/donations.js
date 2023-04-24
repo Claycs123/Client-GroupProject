c2e9a6ebebfa87fd8c166e27c21c584d68a894d5
McLean Standeffer <mstandeffer@McLeans-MacBook-Pro.local>
2023-04-24 17:55:56 -0500

    stuff

--- resources/scripts/donations.js
+++ resources/scripts/donations.js
@@ -1,5 +1,18 @@
-const url = "https://localhost:7060/api/Donations";
+const donationUrl = "https://localhost:7060/api/Donations";
 
+function handleDonationLoad()
+{
+    getDonationData()
+    createDonationTable()
+
+}
+
+async function getDonationData()
+{
+    let response = await fetch(donationUrl)
+    let json = await response.json()
+    return json
+}
 function handleAddDonation()
 {
     console.log("hello")
@@ -17,7 +30,7 @@ function handleAddDonation()
     }
     console.log("2")
     try {
-        var response = fetch(url, {
+        var response = fetch(donationUrl, {
             method: "POST",
             body: JSON.stringify(newDonation),
             headers: {
@@ -36,7 +49,7 @@ function handleAddDonation()
 
 function addRow(donation) {
     
-    let tableBody = document.getElementById("appointmentTableBody");
+    let tableBody = document.getElementById("donationTableBody");
     let tr = document.createElement("TR");
         tableBody.appendChild(tr);
   
@@ -59,20 +72,77 @@ function addRow(donation) {
         let td4 = document.createElement("TD");
         td4.width = 100;
         td4.appendChild(document.createTextNode(`${donation.date}`));
-        tr.appendChild(td4);
-  
-        let td5 = document.createElement("TD");
-        td5.width = 200;
-        td5.appendChild(document.createTextNode(`${appointment.dates}`));
-        tr.appendChild(td5);
-
-        let td6 = document.createElement("TD");
-        td6.width = 200;
-        td6.appendChild(document.createTextNode(`${appointment.timeSlot}`));
-        tr.appendChild(td6);
-    
+        tr.appendChild(td4);  
   }
 
+  async function createDonationTable() {
+    let donations = await getDonationData() || [];
+    let table = document.createElement("TABLE");
+    table.border = "1";
+    console.log("hello")
+    table.id = "donationTable";
+    let tableBody = document.createElement("TBODY");
+    tableBody.id = "donationTableBody";
+    table.appendChild(tableBody);
+  
+    let tr = document.createElement("TR");
+    tableBody.appendChild(tr);
+  
+    let th1 = document.createElement("TH");
+    th1.width = 100;
+    th1.appendChild(document.createTextNode("Email"));
+    tr.appendChild(th1);
+  
+    let th2 = document.createElement("TH");
+    th2.width = 100;
+    th2.appendChild(document.createTextNode("Name"));
+    tr.appendChild(th2);
+  
+    let th3 = document.createElement("TH");
+    th3.width = 200;
+    th3.appendChild(document.createTextNode("Money Donated"));
+    tr.appendChild(th3);
+  
+      let th4 = document.createElement("TH");
+      th4.width = 200;
+      th4.appendChild(document.createTextNode("Date"));
+      tr.appendChild(th4);
+  
+  ;
+  
+    donations.forEach((donation) => {
+       
+        let tr = document.createElement("TR");
+        tableBody.appendChild(tr);
+  
+        let td1 = document.createElement("TD");
+        td1.width = 100;
+        td1.appendChild(document.createTextNode(`${donation.email}`));
+        tr.appendChild(td1);
+  
+        let td2 = document.createElement("TD");
+        td2.width = 100;
+        td2.appendChild(document.createTextNode(`${donation.donorName}`));
+        tr.appendChild(td2);
+  
+        let td3 = document.createElement("TD");
+        td3.width = 200;
+        td3.appendChild(document.createTextNode(`${donation.moneyDonated}`));
+  
+        tr.appendChild(td3);
+  
+        let td4 = document.createElement("TD");
+        td4.width = 200;
+        td4.appendChild(document.createTextNode(`${donation.date}`));
+        tr.appendChild(td4);
+      
+      }
+
+    );
+    console.log(table);
+    document.getElementById("appin").appendChild(table);
+    console.log(donations)
+  }
 
 // const createSong = async (event) => {
 //     event.preventDefault();
